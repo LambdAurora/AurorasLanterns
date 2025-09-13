dedc68a76336be772dc7e6e9292148828c5c01ff
LambdAurora <email@lambdaurora.dev>
2025-09-13 13:47:47 +0200

    Add missing license header.

--- src/client/java/dev/lambdaurora/auroraslanterns/client/renderer/WallLanternBlockEntityRenderState.java
+++ src/client/java/dev/lambdaurora/auroraslanterns/client/renderer/WallLanternBlockEntityRenderState.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright © 2025 LambdAurora <email@lambdaurora.dev>
+ *
+ * This file is part of Aurora's Lanterns.
+ *
+ * Licensed under the Lambda License. For more information,
+ * see the LICENSE file.
+ */
+
 package dev.lambdaurora.auroraslanterns.client.renderer;
 
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
