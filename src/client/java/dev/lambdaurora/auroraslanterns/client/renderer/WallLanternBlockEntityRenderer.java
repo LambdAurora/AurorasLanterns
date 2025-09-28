6d33c146cf8c3e3a6ee6817e68784191bf199f66
LambdAurora <email@lambdaurora.dev>
2025-09-28 14:51:01 +0200

    Port to 1.21.9-rc1.

--- src/client/java/dev/lambdaurora/auroraslanterns/client/renderer/WallLanternBlockEntityRenderer.java
+++ src/client/java/dev/lambdaurora/auroraslanterns/client/renderer/WallLanternBlockEntityRenderer.java
@@ -20,6 +20,7 @@ import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.Direction;
 import net.minecraft.util.math.MathHelper;
@@ -77,7 +78,8 @@ public class WallLanternBlockEntityRenderer
 
 	@Override
 	public void submit(
-			WallLanternBlockEntityRenderState lantern, MatrixStack matrices, SubmitNodeCollector collector
+			WallLanternBlockEntityRenderState lantern, MatrixStack matrices,
+			SubmitNodeCollector collector, CameraRenderState cameraRenderState
 	) {
 		var pos = lantern.blockPos;
 
