c1e04ed35f3fa223264446be97e645ac4d849f94
Layla <email@lambdaurora.dev>
2026-08-16 12:35:05 +0200

    Update buildscript, add theoretical NeoForge support.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
-import com.modrinth.minotaur.dependencies.ModDependency
+import dev.lambdaurora.mcdev.api.EnvironmentType
 import dev.lambdaurora.mcdev.api.McVersionLookup
 import dev.lambdaurora.mcdev.api.ModUtils
 import dev.lambdaurora.mcdev.api.ModVersionDependency
+import dev.lambdaurora.mcdev.api.manifest.MixinEntry
+import dev.lambdaurora.mcdev.api.manifest.ModEnvironment
+import dev.lambdaurora.mcdev.task.ConvertAccessWidenerToTransformer
+import dev.lambdaurora.mcdev.task.GenerateNeoForgeJiJDataTask
 import dev.lambdaurora.mcdev.task.packaging.PackageModrinthTask
-import net.darkhax.curseforgegradle.TaskPublishCurseForge
 
 plugins {
 	alias(libs.plugins.loom)
@@ -11,8 +14,6 @@ plugins {
 	alias(libs.plugins.licenser)
 	`java-library`
 	`maven-publish`
-	id("com.modrinth.minotaur").version("2.+")
-	id("net.darkhax.curseforgegradle").version("1.1.+")
 }
 
 lambdamcdev.namespace.set(project.property("mod_namespace") as String)
@@ -21,6 +22,7 @@ base.archivesName.set(lambdamcdev.namespace)
 val mcVersion = libs.versions.minecraft.get()
 val compatibleMcVersions: Set<String> = setOf()
 val VERSION = project.property("mod_version") as String
+val supportNeoforge = (project.property("support_neoforge") as String).toBoolean()
 version = "$VERSION+$mcVersion"
 
 // This field defines the Java version your mod target.
@@ -74,6 +76,71 @@ java {
 	withSourcesJar()
 }
 
+lambdamcdev {
+	manifests {
+		fmj {
+			val sourcesLink = "https://github.com/LambdAurora/AurorasLanterns"
+
+			withName(project.property("mod_name") as String)
+			withDescription(project.property("mod_description") as String)
+			withAuthors("LambdAurora")
+			withContact {
+				it.withHomepage("https://lambdaurora.dev/projects/auroraslanterns")
+					.withSources("$sourcesLink.git")
+					.withIssues("$sourcesLink/issues")
+			}
+			withLicense("Lambda License")
+			withIcon("assets/${namespace.get()}/icon.png")
+			withEnvironment("*")
+			withEntrypoints("yumi:init", "dev.lambdaurora.auroraslanterns.AurorasLanterns")
+			withEntrypoints("yumi:client_init", "dev.lambdaurora.auroraslanterns.client.AurorasLanternsClient")
+			withEntrypoints("fabric-datagen", "dev.lambdaurora.auroraslanterns.client.resource.datagen.AurorasLanternsStaticDatagen")
+			withEntrypoints(
+				"fabric-gametest",
+				"dev.lambdaurora.auroraslanterns.test.RedstoneLanternTest",
+				"dev.lambdaurora.auroraslanterns.test.WallLanternTest",
+			)
+			withAccessWidener("${namespace.get()}.classtweaker")
+			withMixins(
+				MixinEntry("${namespace.get()}.mixins.json"),
+				MixinEntry("${namespace.get()}.client.mixins.json", ModEnvironment.CLIENT),
+			)
+			withDepend("fabricloader", ">=${libs.versions.fabric.loader.get()}")
+			withDepend("minecraft", project.property("fabric_mc_constraints").toString())
+			withDepend("java", ">=$targetJavaVersion")
+			withDepend("yumi_mc_core", ">=${libs.versions.yumi.mc.foundation.get()}")
+			withDepend("fabric-api", ">=${libs.versions.fabric.api.get()}")
+			withModMenu {
+				it.withCurseForge("https://www.curseforge.com/minecraft/mc-mods/auroraslanterns")
+					.withDiscord("https://discord.lambdaurora.dev/")
+					.withGitHubReleases("$sourcesLink/releases")
+					.withModrinth("https://modrinth.com/mod/auroraslanterns")
+					.withLink("modmenu.bluesky", "https://bsky.app/profile/lambdaurora.dev")
+					.withLink("modmenu.donate", "https://donate.lambdaurora.dev/")
+			}
+		}
+
+		if (supportNeoforge) {
+			val fmj = this.fmj().get()
+
+			nmt {
+				fmj.copyTo(this)
+				withLoaderVersion("[2,)")
+				withBlurIcon(false)
+				withYumiEntrypoints("yumi:init", "dev.lambdaurora.auroraslanterns.AurorasLanterns")
+				withYumiEntrypoints("yumi:client_init", "dev.lambdaurora.auroraslanterns.client.AurorasLanternsClient")
+				withAccessTransformer("META-INF/accesstransformer.cfg")
+				withMixins("${namespace.get()}.mixins.json", "${namespace.get()}.client.mixins.json")
+				withDepend("minecraft", project.property("neoforge_mc_constraints").toString())
+				withDepend("yumi_mc_core", "[${libs.versions.yumi.mc.foundation.get()},)")
+				withDepend("fabric_api", "[${libs.versions.fabric.api.get()},)")
+			}
+		}
+	}
+
+	setupActionsRefCheck()
+}
+
 tasks.withType<JavaCompile>().configureEach {
 	options.encoding = "UTF-8"
 	options.isDeprecation = true
@@ -105,17 +172,58 @@ license {
 	include("**/*.java")
 }
 
+val convertAWtoATTask = tasks.register("convertAWtoAT", ConvertAccessWidenerToTransformer::class) {
+	this.group = "generation"
+	this.enabled = supportNeoforge
+	this.input = loom.accessWidenerPath
+	this.output = project.layout.buildDirectory.get().file("generated/accesstransformer.cfg")
+}
+
+val generateJarJarMetadataTask = tasks.register<GenerateNeoForgeJiJDataTask>("generateJarJarMetadata") {
+	this.enabled = supportNeoforge
+	val includeConfig = project.configurations.getByName("includeInternal");
+	this.from(includeConfig)
+	this.outputFile.set(
+		project.layout.buildDirectory
+			.asFile
+			.map(File::toPath)
+			.map { path -> path.resolve("generated/jarjar/metadata.json").toFile() }
+			.get()
+	)
+}
+
+if (supportNeoforge) {
+	tasks.named<Jar>("jar") {
+		from(generateJarJarMetadataTask.map { it.outputFile }) {
+			into("META-INF/jarjar")
+		}
+		from(convertAWtoATTask) {
+			into("META-INF")
+		}
+	}
+
+	tasks.named<Jar>("sourcesJar") {
+		this.from(convertAWtoATTask) {
+			into("META-INF")
+		}
+	}
+}
+
 val README = ModUtils.parseReadme(
 	project, "https://raw.githubusercontent.com/LambdAurora/AurorasLanterns/26.1/\$2"
 )
 val CHANGELOG_CONTENT = ModUtils.fetchChangelog(project, VERSION)
 
-val packageModrinth by tasks.registering(PackageModrinthTask::class) {
+tasks.register<PackageModrinthTask>("packageModrinth") {
 	this.group = "publishing"
 	this.versionType.set(ModUtils.getVersionType(VERSION, mcVersion))
 	this.versionName.set("${project.property("mod_name")} $VERSION (${McVersionLookup.getVersionTag(mcVersion)})")
 	this.gameVersions.set(listOf(mcVersion) + compatibleMcVersions)
 	this.loaders.set(listOf("fabric", "quilt"))
+	if (supportNeoforge) {
+		this.loaders.add("neoforge")
+	}
+	this.environment.set(EnvironmentType.CLIENT_AND_SERVER)
 	this.dependencies.set(
 		listOf(
 			ModVersionDependency("P7dR8mSH", ModVersionDependency.Type.REQUIRED), // Fabric API
@@ -124,74 +232,6 @@ val packageModrinth by tasks.registering(PackageModrinthTask::class) {
 	this.changelog.set(CHANGELOG_CONTENT)
 	this.readme.set(README)
 	this.files.setFrom(tasks.jar.get())
-}
-
-modrinth {
-	projectId.set(project.property("modrinth_id") as String)
-	versionName.set("${project.property("mod_name")} $VERSION (${McVersionLookup.getVersionTag(mcVersion)})")
-	versionType.set(ModUtils.fetchVersionType(VERSION, mcVersion))
-	uploadFile.set(tasks.jar)
-	loaders.set(listOf("fabric", "quilt"))
-	gameVersions.set(listOf(mcVersion) + compatibleMcVersions)
-	dependencies.set(
-		listOf(
-			ModDependency("P7dR8mSH", "required") // Fabric API
-		)
-	)
-	syncBodyFrom.set(README)
-
-	// Changelog fetching
-	if (CHANGELOG_CONTENT != null) {
-		changelog.set(CHANGELOG_CONTENT)
-	} else {
-		afterEvaluate {
-			tasks.modrinth.get().isEnabled = false
-		}
-	}
-
-	// If we don't have a MODRINTH_TOKEN, don't run the modrinth publish tasks.
-	if (System.getenv("MODRINTH_TOKEN") == null) {
-		project.logger.debug("MODRINTH_TOKEN is not set! Disabled modrinth and modrinthSyncBody tasks.")
-		tasks.modrinth.get().isEnabled = false
-		tasks.modrinthSyncBody.get().isEnabled = false
-	}
-}
-
-tasks.register<TaskPublishCurseForge>("curseforge") {
-	this.group = "publishing"
-
-	val token = System.getenv("CURSEFORGE_TOKEN")
-	if (token != null) {
-		this.apiToken = token
-	} else {
-		this.isEnabled = false
-		return@register
-	}
-
-	// Changelog fetching
-	var changelogContent = CHANGELOG_CONTENT
-
-	if (changelogContent != null) {
-		changelogContent = "Changelog:\n\n${changelogContent}"
-	} else {
-		this.isEnabled = false
-		return@register
-	}
-
-	val mainFile = upload(project.property("curseforge_id"), tasks.jar.get())
-	mainFile.releaseType = ModUtils.fetchVersionType(VERSION, mcVersion)
-	mainFile.addGameVersion(McVersionLookup.getCurseForgeEquivalent(mcVersion))
-	compatibleMcVersions.stream()
-		.map { McVersionLookup.getCurseForgeEquivalent(it) }
-		.forEach { mainFile.addGameVersion(it) }
-	mainFile.addModLoader("Fabric", "Quilt")
-	mainFile.addJavaVersion("Java 21", "Java 22")
-
-	mainFile.displayName = "${project.property("mod_name")} $VERSION (${McVersionLookup.getVersionTag(mcVersion)})"
-	mainFile.addRequirement("fabric-api")
-
-	mainFile.changelogType = "markdown"
-	mainFile.changelog = changelogContent
 }
 
 publishing {
